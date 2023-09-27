70592371e6d59162a7ff3cb9814cb8aa6170859d
casper <casperjhs@gmail.com>
2023-09-27 16:03:19 +0200

    Scripts to create edits working correctly

--- validation testing/Edit graph/create_edits.py
+++ validation testing/Edit graph/create_edits.py
@@ -23,7 +23,7 @@ a) Length of edit - range of length values the edits can take
 b) Distribution - vague, but we need to decide how the edits are allowed to be distributed. No edits within
     n base pairs of the start or end of chromosome, for example.
 
-c) Types of edits - Substitution, Deletion, or Insertion. Frequency for each can be a weight selected. For testing we
+c) Types of edits - Substitution, Deletion, or Insertion. Frequency for each can be a weight selected. For testing, we
     can make all of them equally likely.
 
 d) Number of edits - the user can specify how many edits should be generated.
@@ -34,32 +34,38 @@ import random
 from Bio import SeqIO
 
 # Number of edits to generate (user-defined)
-num_edits = 50
+num_edits = 5
 
 # Input FASTA file (provide the actual path, eventually this will be a user-defined argument)
-fasta_file = "/home/casper/PycharmProjects/fantastic-lamp/validation testing/reference genome/lambda_phage.fasta"
+# fasta_file = "/home/casper/PycharmProjects/fantastic-lamp/validation testing/reference genome/lambda_phage.fasta"
+fasta_file = "/home/casper/PycharmProjects/fantastic-lamp/validation testing/reference genome/tiny_test.fasta"
 
 
 # Function to load the reference genome from a FASTA file
 def load_reference_genome(fasta_file):
+    """
+    This function loads the reference genome from a FASTA file and returns the sequence as a string, and its length.
+    """
     for record in SeqIO.parse(fasta_file, "fasta"):
         return record.seq, len(record.seq)
 
 
 # Define edit types and their weights
 edit_types = ["Substitution", "Deletion", "Insertion"]
-edit_weights = [1, 1, 1]  # Equal weights for testing
-bps = ["A", "T", "G", "C"]  # base pairs for random edit creation
+
+edit_weights = [1, 1, 1]  # These weights can be adjusted to change the frequency of each edit type
+
+bps = ["A", "T", "G", "C"]
+# base pairs for random edit creation, can be adjusted to include Ns or other characters for testing
 
 
 def generate_substitution(start_pos, stop_pos, length):
     """
     This function generates an edit of the substitution type. The reference sequence in the edit range will be
-    replaced by a different sequence that for testing purposes will be a sequence of "N"s. Will take
-    edit parameters as arguments and use these to create the edit.
+    replaced by a different sequence of the same length.
     """
     es = ""  # edit sequence
-    bp = random.choices(bps, k=length)
+    bp = random.choices(bps, k=length)  # Creates a random sequence of base pairs to use as the edit sequence
     es = es.join(bp)
     return es
 
@@ -67,7 +73,7 @@ def generate_substitution(start_pos, stop_pos, length):
 def generate_deletion(start_pos, stop_pos, length):
     """
     This function generates a deletion edit. The reference sequence in the edit range will be deleted. Will take
-    edit parameters as arguments and use these to create the edit.
+    edit parameters as arguments and use these to create the edit. The output of this function is NOT currently used.
     """
     # Determine the length of the deletion
     deletion_length = stop_pos - start_pos + 1
@@ -85,7 +91,7 @@ def generate_insertion(start_pos, stop_pos, length):
     edit parameters as arguments and use these to create the edit.
     """
     es = ""  # edit sequence
-    bp = random.choices(bps, k=length)
+    bp = random.choices(bps, k=length)  # Creates a random sequence of base pairs to use as the edit sequence
     es = es.join(bp)
     return es
 
@@ -93,18 +99,22 @@ def generate_insertion(start_pos, stop_pos, length):
 # Function to generate a random edit record
 
 def generate_edit(chromosome_length):
+    edit_min = 10  # Minimum length of edit
+    edit_max = 20  # Maximum length of edit
+    margin = 200  # This is how close to the start or end of the chromosome an edit can be. For obvious reasons,
+    # the edit cannot be closer to the start or end of a chromosome than the length of the edit itself.
+
     edit = {
-        "ID": "ID-XXXX",  # Will generate sequential IDs
+        "ID": "ID-XXXX",  # Sequential IDs are added in a function lower down.
         "Edit type": random.choices(edit_types, weights=edit_weights)[0],
-        "Start": random.randint(100, chromosome_length - 100),
+        "Start": random.randint(margin, chromosome_length - margin),
     }
+
     # Generate a random length within a specific range
-    edit_length = random.randint(6, 60)  # Adjust the range as needed
+    edit_length = random.randint(edit_min, edit_max)  # Adjust the range as needed
     # Calculate the stop position based on the start and length
     edit["Stop"] = edit["Start"] + edit_length
     return edit
-
-
 
 
 # Load reference genome and its length
@@ -114,58 +124,74 @@ reference_sequence, chromosome_length = load_reference_genome(fasta_file)
 def generate_homology_arm_sequences(reference_sequence, edit_type, start_pos, stop_pos, edit_sequence):
     """
     This function generates the HomologyArmRefSeq and HomologyArmEditSeq fields based on the edit type and positions.
+    For details on how each edit is created, see the docstrings in the if statements below.
     """
     homology_arm_ref = 0
     homology_arm_edit = 0
-    # if edit_type == "Deletion":
-    #     field_len = 138
-    # else:
-    #     field_len = 138 - len(edit_sequence)
+
     field_len = 138
+    # Change this to adjust the length of the homology arms. In some cases they are longer than this
+    # to account for the edit, but this is the minimum. The length of the homology can vary by 1bp.
+
     flank_length = field_len // 2  # You can adjust this value based on your requirements
 
     if edit_type == "Insertion":
-        # Extract the left and right flanking sequences from the reference genome
-        left_flank = str(reference_sequence[start_pos - flank_length - 1: start_pos - 1])  # Adjust for 0-based indexing
-        right_flank = str(reference_sequence[stop_pos: stop_pos + flank_length])
-        # For insertions, the edit sequence is inserted between the flanking sequences
-        homology_arm_ref = left_flank + '-' * len(edit_sequence) + right_flank
-        homology_arm_edit = left_flank + edit_sequence + right_flank
+        """
+        homology_arm_ref: flank - flank (138bp)
+        homology_arm_edit: flank - inserted seq - flank (138 + inserted seq length)
+        """
+        edit_pos = len(edit_sequence) // 2
+
+        homology_arm_ref = str(
+            reference_sequence[start_pos - flank_length + edit_pos: start_pos + flank_length + edit_pos])
+
+        start_pos = start_pos + edit_pos
+
+        homology_arm_edit = (str(reference_sequence[start_pos - flank_length: start_pos]) + edit_sequence +
+                             str(reference_sequence[start_pos: start_pos + flank_length]))
 
     elif edit_type == "Substitution":
-        # Extract the left and right flanking sequences from the reference genome
-        left_flank = str(reference_sequence[start_pos - flank_length - 1: start_pos - 1])  # Adjust for 0-based indexing
-        right_flank = str(reference_sequence[stop_pos: stop_pos + flank_length])
-        # For substitutions and the edit sequence replaces the reference sequence
-        homology_arm_ref = left_flank + reference_sequence[start_pos - 1: stop_pos] + right_flank
-        homology_arm_edit = left_flank + edit_sequence + right_flank
+        """
+        homology_arm_ref: flank - ref seq - flank (138bp)
+        homology_arm_edit: flank - edit seq (replaces ref seq) - flank (138bp)
+        """
+        edit_pos = len(edit_sequence) // 2
+        homology_arm_ref = str(
+            reference_sequence[start_pos - flank_length + edit_pos: start_pos + flank_length + edit_pos])
+
+        homology_arm_edit = (str(reference_sequence[start_pos - flank_length + edit_pos: start_pos - 1])
+                             + edit_sequence + str(reference_sequence[stop_pos: stop_pos + flank_length - edit_pos]))
 
     elif edit_type == "Deletion":
-        # For deletions, extend the flanking sequences to meet the required lengths
-        # Extract the left and right flanking sequences from the reference genome
-        left_flank = str(reference_sequence[start_pos - flank_length - 1 - len(edit_sequence): start_pos - 1])
-        # Adjusted for 0-based indexing
-        if field_len % 2 == 0:  # This ensures that the length of the fields are consistent.
-            right_flank = str(reference_sequence[stop_pos + len(edit_sequence): stop_pos + flank_length])
-        else:
-            right_flank = str(reference_sequence[stop_pos + len(edit_sequence): stop_pos + flank_length + 1])
+        """
+        homology_arm_ref: flank - ref seq - flank (138 + deletion length)
+        homology_arm_edit: flank - (ref seq removed) - flank (138bp)
+        """
+        # If you want to extend the ref arm for deletions, shift the start and stop by the edit_pos.
+        # edit_pos = len(edit_sequence) // 2
+        homology_arm_ref = str(reference_sequence[start_pos - flank_length:
+                                                  stop_pos + flank_length])
 
-        homology_arm_ref = left_flank + reference_sequence[start_pos:stop_pos] + right_flank
-        homology_arm_edit = left_flank + right_flank
+        homology_arm_edit = (str(reference_sequence[start_pos - flank_length: start_pos - 1]) +
+                             str(reference_sequence[stop_pos: stop_pos + flank_length]))
 
+    print(
+        f"Edit type: {edit_type}, homology_arm_ref: {len(homology_arm_ref)}, homology_arm_edit: {len(homology_arm_edit)}")
     return homology_arm_ref, homology_arm_edit
 
 
 # Generate and store edit records
 edits = []
-for _ in range(num_edits):
+for x in range(num_edits):
     edit = generate_edit(chromosome_length)
     start = edit["Start"]
     stop = edit["Stop"]
 
     if edit["Edit type"] == "Substitution":
         edit_sequence = generate_substitution(start, stop, stop - start + 1)
+
     elif edit["Edit type"] == "Deletion":
+
         edit_sequence = generate_deletion(start, stop, stop - start + 1)
     elif edit["Edit type"] == "Insertion":
         edit_sequence = generate_insertion(start, stop, stop - start + 1)
@@ -173,15 +199,16 @@ for _ in range(num_edits):
     homology_arm_ref, homology_arm_edit = generate_homology_arm_sequences(reference_sequence, edit["Edit type"],
                                                                           start, stop, edit_sequence)
 
-    edit["ID"] = _ + 1
-    edit["Ref sequence"] = str(reference_sequence[start - 1: stop])  # Adjust for 0-based indexing
+    edit["ID"] = x + 1
+    edit["Ref sequence"] = str(reference_sequence[start - 1: stop])  # Adjusted for 0-based indexing
     if edit["Edit type"] == "Deletion":
         edit["Edit sequence"] = ""
     else:
         edit["Edit sequence"] = edit_sequence
-    edit["HomologyArmRefSeq"] = homology_arm_ref  # THE REF SEQ IS NOT CONTAINED WITHIN
+    edit["HomologyArmRefSeq"] = homology_arm_ref
     edit["HomologyArmEditSeq"] = homology_arm_edit
     # edit["Edit length"] = len(edit_sequence)
+    # for testing
     edits.append(edit)
 
 # Write edits to a CSV file
